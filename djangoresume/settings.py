25f94019bf949ce3ffeda30beb37ae37bd7fa6fa
Tunde Alabi <tundealabi4780@gmail.com>
2021-08-22 01:24:52 +0100

    test one

--- djangoresume/settings.py
+++ djangoresume/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 #     os.path.join(BASE_DIR, 'static'), 
 # ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
 
